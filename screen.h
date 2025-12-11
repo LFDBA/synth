cd6aa528bd285ffd49dcb7f3673a2e697e75e731
Fynn <fnbisschoff@gmail.com>
2025-12-11 19:03:59 +1300

    update lol

--- screen.h
+++ screen.h
@@ -4,4 +4,4 @@ void drawLine(int x0, int y0, int x1, int y1);
 void drawPixel(int x, int y);
 void clearBuffer();
 void updateDisplay(int spi);
-void drawADSR();
+void drawADSR(float attack, float decay, float sustain, float release);
