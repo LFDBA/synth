13de3160f8a16d44e8d2b4c65382166724905917
Fynn <fnbisschoff@gmail.com>
2025-12-12 16:49:27 +1300

    17?

--- keys.cpp
+++ keys.cpp
@@ -71,24 +71,6 @@ int main() {
             std::this_thread::sleep_for(std::chrono::microseconds(50));
         }
         gpioWrite(17, 0);
-        gpioWrite(27, 1);
-        for(size_t inIdx = 0; inIdx < pins.size(); ++inIdx){
-            if(inIdx == 4 || inIdx == 5) continue;
-            if(gpioRead(pins[inIdx]) == 1){
-                std::cout << "Key pressed: " << inIdx + 49 << std::endl;
-            }
-            std::this_thread::sleep_for(std::chrono::microseconds(50));
-        }
-        gpioWrite(27, 0);
-        gpioWrite(22, 1);
-        for(size_t inIdx = 0; inIdx < pins.size(); ++inIdx){
-            if(inIdx == 5 || inIdx == 6) continue;
-            if(gpioRead(pins[inIdx]) == 1){ 
-                std::cout << "Key pressed: " << inIdx + 61 << std::endl;
-            }
-            std::this_thread::sleep_for(std::chrono::microseconds(50));
-        }
-        gpioWrite(22, 0);
         
         
         
