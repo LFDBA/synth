0c44361d85921ba22086a4758ad44bba84b84b82
Fynn <fnbisschoff@gmail.com>
2025-12-14 20:16:10 +1300

    ugh

--- main.cpp
+++ main.cpp
@@ -287,9 +287,9 @@ void drawSanta(int cx, int cy, float fatness) {
     drawCircle(cx, cy - 12, 8);
 
     // Hat
-    drawRectFilled(cx - 10, cy - 20, 20, 6);
-    drawLine(cx -5, cy-20, cx+7, cy-28);
-    drawLine(cx -15, cy-20, cx+9, cy-28);
+    drawRectFilled(cx - 10, cy - 22, 20, 6);
+    drawLine(cx -5, cy-22, cx+7, cy-28);
+    drawLine(cx, cy-22, cx+9, cy-28);
     drawCircle(cx + 8, cy - 30, 3); // pompom
 
     // Beard
