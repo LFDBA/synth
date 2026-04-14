d20fa4568eb5077ee6bbf50d9421f3b0a5d724c6
ginkus <scratchpoodle1679@gmail.com>
2026-04-15 10:17:51 +1200

    higher failure rate?

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ using namespace std::chrono;
 
 
 
-std::vector<int> pins = {4, 5, 6, 12, 13, 17, 18, 19, 20, 23, 22};
+std::vector<int> pins = {4, 5, 6, 12, 13, 17, 18, 19, 20, 22, 23};
 
 unsigned long lastClickTime = 0;
 const unsigned long doubleClickDelay = 400; // ms
@@ -896,44 +896,28 @@ int getKeyPress() {
 
 int readKeyBoard() {
     const size_t n = pins.size();
-    bool anyHigh = false;
 
     for (size_t i = 0; i < n; ++i) {
+        // Drive this pin high
         gpioSetMode(pins[i], PI_OUTPUT);
         gpioWrite(pins[i], 1);
-        std::this_thread::sleep_for(std::chrono::microseconds(500));
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
 
         for (size_t j = 0; j < n; ++j) {
-            if (j == i) continue;
-            int keyNum = (i * n) + j + 1;
-            bool isHigh = gpioRead(pins[j]) == 1;
-
-            if (isHigh) {
-                anyHigh = true;
-                keyStates[keyNum].count++;
-                if (keyStates[keyNum].count >= debounceScans && !keyStates[keyNum].pressed) {
-                    keyStates[keyNum].pressed = true;
-                    gpioWrite(pins[i], 0);
-                    gpioSetMode(pins[i], PI_INPUT);
-                    gpioSetPullUpDown(pins[i], PI_PUD_DOWN);
-                    return keyNum;
-                }
-            } else {
-                keyStates[keyNum].count = 0;
-                if (keyStates[keyNum].pressed) {
-                    keyStates[keyNum].pressed = false;
-                    gpioWrite(pins[i], 0);
-                    gpioSetMode(pins[i], PI_INPUT);
-                    gpioSetPullUpDown(pins[i], PI_PUD_DOWN);
-                    return -keyNum - 1;
-                }
+            if (j == i) continue; // don't read the pin we're driving
+            if (gpioRead(pins[j]) == 1) {
+                // Reset before returning
+                gpioWrite(pins[i], 0);
+                gpioSetMode(pins[i], PI_INPUT);
+                gpioSetPullUpDown(pins[i], PI_PUD_DOWN);
+                return (i * n) + j + 1;
             }
         }
 
+        // Reset pin back to input
         gpioWrite(pins[i], 0);
         gpioSetMode(pins[i], PI_INPUT);
         gpioSetPullUpDown(pins[i], PI_PUD_DOWN);
-        std::this_thread::sleep_for(std::chrono::microseconds(100));
     }
 
     return -1111;
