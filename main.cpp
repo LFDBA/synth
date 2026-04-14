fd895ba8ec2a6b980ad76f2afaaca463ca468d57
ginkus <scratchpoodle1679@gmail.com>
2026-04-15 09:11:27 +1200

    maybe not (just brute forcing now)

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ using namespace std::chrono;
 
 
 
-std::vector<int> pins = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
+std::vector<int> pins = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
 
 unsigned long lastClickTime = 0;
 const unsigned long doubleClickDelay = 400; // ms
