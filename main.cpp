f92859d2c88c3268a6604365467f899d7d469c85
ginkus <scratchpoodle1679@gmail.com>
2026-04-15 13:38:42 +1200

    add pins = {...}

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ using namespace std::chrono;
 #include <iostream>
 
 
-
+std::vector<int> pins = {4, 5, 6, 12, 13, 17, 18, 19, 20, 22, 23};
 std::vector<int> rowPins = {4, 5, 6, 12}; 
 std::vector<int> colPins = {13, 17, 18, 19, 20, 22, 23};
 
