bcea3cbff1154f6c27b0cdb74bfd37476af9e0b8
ginkus <scratchpoodle1679@gmail.com>
2026-04-16 12:04:04 +1200

    loud?

--- main.cpp
+++ main.cpp
@@ -563,7 +563,7 @@ int mapKeyNumber(int k) {
 // ======================================================
 
 const int numVoices = 7;
-float outputLevel = 0.1f;
+float outputLevel = 1.0f;
 bool normVoices = true; // Normalize by active voices
 int fd;
 int editIndex = 0;
